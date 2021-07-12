bc31c1732a532fa821586754b6e35d58f9cf6994
Dean Agan <agandfr@gmail.com>
2021-07-12 17:28:32 +1000

    Add a add repair modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,30 +4,30 @@ import { RepairOverview } from "./views/RepairOverview";
 import { NavBar } from "./views/NavBar";
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { bindActionCreators } from "redux";
 import { actionCreators, State } from "./store";
+import { AddRepairModal } from "./views/AddRepairModal";
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
-  const { addRepair, updateRepairState } = bindActionCreators(actionCreators, dispatch);
   const state = useSelector((state: State) => state.repair);
 
   useEffect(() => {
-      dispatch(actionCreators.getRepairEntries())
-  }, [dispatch])
+    dispatch(actionCreators.getRepairEntries());
+  }, [dispatch]);
 
   return (
     <div className="App">
       <NavBar />
       <div>
-        <RepairOverview {...state}/>
+        <RepairOverview {...state} />
       </div>
-      <div>
-        <button onClick={() => addRepair({name: "Car Handle", detail: "handle got chipped", fixed: false})}>Add</button>
-        <button onClick={() => updateRepairState(1, true)}>Update</button>
+      {/* <button onClick={() => addRepair({name: "Car Handle", detail: "handle got chipped", fixed: false})}>Add</button> */}
+      {/* <Col sm={3}><Button variant="primary" onClick={() => updateRepairState(1, true)}>Update</Button></Col> */}
+      <div className="float-md-right mr-4" >
+        <AddRepairModal />
       </div>
     </div>
   );
-}
+};
 
 export default App;
