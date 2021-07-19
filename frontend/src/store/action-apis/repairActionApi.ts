9e5a2256860ea95a4274fef8b9740a000a897a2e
Dean Agan <agandfr@gmail.com>
2021-07-19 20:16:34 +1000

    Create a wrapper around axios

---
+++ frontend/src/store/action-apis/repairActionApi.ts
@@ -0,0 +1,59 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { IHttpClient, IHttpClientRequestParameters } from "../../types";
+
+export class HttpClient implements IHttpClient {
+  post<T>(parameters: IHttpClientRequestParameters<T>): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      const { url, payload, requiresToken } = parameters;
+
+      // axios request options like headers etc
+      const options: AxiosRequestConfig = {
+        headers: {},
+      };
+
+      // if API endpoint requires a token, we'll need to add a way to add this.
+      if (requiresToken) {
+    //     const token = this.getToken();
+    //     options.headers.RequestVerificationToken = token;
+      }
+
+      // finally execute the GET request with axios:
+      axios
+        .post(url, payload, options)
+        .then((response: any) => {
+          resolve(response.data as T);
+        })
+        .catch((response: any) => {
+          reject(response);
+        });
+    });
+  }
+
+  get<T>(parameters: IHttpClientRequestParameters<T>): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      // extract the individual parameters
+      const { url, requiresToken } = parameters;
+
+      // axios request options like headers etc
+      const options: AxiosRequestConfig = {
+        headers: {},
+      };
+
+      // if API endpoint requires a token, we'll need to add a way to add this.
+      if (requiresToken) {
+        // const token = this.getToken();
+        // options.headers.RequestVerificationToken = token;
+      }
+
+      // finally execute the GET request with axios:
+      axios
+        .get(url, options)
+        .then((response: any) => {
+          resolve(response.data as T);
+        })
+        .catch((response: any) => {
+          reject(response);
+        });
+    });
+  }
+}
