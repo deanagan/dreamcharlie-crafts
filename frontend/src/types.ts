3d93c00e9e8223be805abac6448b72687195ffec
Dean Agan <agandfr@gmail.com>
2021-07-19 20:16:17 +1000

    Add interfaces to make http client generic

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -11,3 +11,14 @@ export interface RepairState {
     readonly repairs: Repair[];
     readonly errors?: string;
 }
+
+export interface IHttpClientRequestParameters<T> {
+    url: string
+    requiresToken: boolean
+    payload?: T
+}
+
+export interface IHttpClient {
+    get<T>(parameters: IHttpClientRequestParameters<T>): Promise<T>
+    post<T>(parameters: IHttpClientRequestParameters<T>): Promise<T>
+}
