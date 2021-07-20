3aa74cf49fe51cb5a532e09a33b207e617798fab
Dean Agan <agandfr@gmail.com>
2021-07-20 18:51:51 +1000

    Add put implementation

--- frontend/src/store/action-apis/repairActionApi.ts
+++ frontend/src/store/action-apis/repairActionApi.ts
@@ -29,6 +29,33 @@ export class HttpClient implements IHttpClient {
     });
   }
 
+  put<T>(parameters: IHttpClientRequestParameters<T>): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      const { url, payload, requiresToken } = parameters;
+
+      // axios request options like headers etc
+      const options: AxiosRequestConfig = {
+        headers: {},
+      };
+
+      // if API endpoint requires a token, we'll need to add a way to add this.
+      if (requiresToken) {
+    //     const token = this.getToken();
+    //     options.headers.RequestVerificationToken = token;
+      }
+
+      // finally execute the GET request with axios:
+      axios
+        .put(url, {...payload}, options)
+        .then((response: any) => {
+          resolve(response.data as T);
+        })
+        .catch((response: any) => {
+          reject(response);
+        });
+    });
+  }
+
   get<T>(parameters: IHttpClientRequestParameters<T>): Promise<T> {
     return new Promise<T>((resolve, reject) => {
       // extract the individual parameters
