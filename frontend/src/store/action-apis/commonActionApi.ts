8e1dd0526924bc6b360bd9bdd18f47d31a05840f
Dean Agan <agandfr@gmail.com>
2021-07-21 18:47:29 +1000

    Add delete api wrapper

--- frontend/src/store/action-apis/commonActionApi.ts
+++ frontend/src/store/action-apis/commonActionApi.ts
@@ -56,6 +56,33 @@ export class HttpClient implements IHttpClient {
     });
   }
 
+  delete<T>(parameters: IHttpClientRequestParameters<T>): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      const { url, requiresToken } = parameters;
+
+      // axios request options like headers etc
+      const options: AxiosRequestConfig = {
+        headers: {},
+      };
+
+      // if API endpoint requires a token, we'll need to add a way to add this.
+      if (requiresToken) {
+    //     const token = this.getToken();
+    //     options.headers.RequestVerificationToken = token;
+      }
+
+      // finally execute the GET request with axios:
+      axios
+        .delete(url, options)
+        .then((response: any) => {
+          resolve(response.data as T);
+        })
+        .catch((response: any) => {
+          reject(response);
+        });
+    });
+  }
+
   get<T>(parameters: IHttpClientRequestParameters<T>): Promise<T> {
     return new Promise<T>((resolve, reject) => {
       // extract the individual parameters
