0c4910ab66d04dafbf7aeb3185f3332516439933
Dean Agan <agandfr@gmail.com>
2021-08-14 16:22:25 +1000

    Rename to action link

---
+++ frontend/src/components/ActionLink.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import styled from "styled-components";
+
+interface ActionLinkProp {
+  deleteFn(): any;
+  message: string;
+  color: string;
+}
+
+const ActionLinkWrapper = styled.div`
+  color:  ${({ color = 0 }) => color};
+  text-decoration: underline;
+  cursor: pointer;
+  margin: auto;
+
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+export const ActionLink: FC<ActionLinkProp> = ({ color, deleteFn, message }) => {
+  return (
+    <>
+      <ActionLinkWrapper color={color} onClick={() => deleteFn()}>{message}</ActionLinkWrapper>
+    </>
+  );
+};
