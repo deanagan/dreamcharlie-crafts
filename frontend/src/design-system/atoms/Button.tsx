c5b35286408bfd3d9c3ffc2fb629c9521d7b33d0
Dean Agan <agandfr@gmail.com>
2021-08-15 12:15:02 +1000

    Creating button

---
+++ frontend/src/design-system/atoms/Button.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+interface ButtonProp {
+    onClick: any;
+}
+
+export const ButtonWrapper = styled.button`
+  padding: 15px 25px;
+  font-size: 12px;
+  text-align: center;
+  cursor: pointer;
+  outline: none;
+  color: white;
+  background-color: #04aa6d;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 5px #999;
+  float: right;
+  margin-top: 2px;
+
+  &:hover {
+    background-color: #032903;
+    color: white;
+  }
+
+  &:active {
+    background-color: #3e8e41;
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
+`;
+
+export const Button: React.FC<ButtonProp> = ({ children, onClick }) => {
+    return (
+        <ButtonWrapper onClick={onClick}>
+            {children}
+        </ButtonWrapper>
+    );
+};
