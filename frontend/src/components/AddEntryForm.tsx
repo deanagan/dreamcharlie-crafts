91d64507be2057839f8f02c0dbd9819ad3ce45a5
Dean Agan <agandfr@gmail.com>
2021-08-17 19:44:41 +1000

    Add entry form

--- frontend/src/components/AddEntryForm.tsx
+++ frontend/src/components/AddEntryForm.tsx
@@ -1,5 +1,37 @@
 import { useState } from "react";
-import { Form } from "react-bootstrap";
+import styled from "styled-components";
+
+export const FormGroup = styled.div`
+	color: palevioletred;
+    display: block;
+	width: 90%;
+	margin: 10px;
+`;
+
+export const Label = styled.label`
+	margin-bottom: 0.5em;
+	color: palevioletred;
+    display: block;
+`;
+
+
+export const Input = styled.input`
+	padding: 0.5em;
+	color: palevioletred;
+	background: papayawhip;
+	border: none;
+	border-radius: 3px;
+	width: 100%;
+  max-width: 100%;
+	margin-bottom: 0.5em;
+`;
+
+export const Message = styled.label`
+	margin-bottom: 0.5em;
+	color: palevioletred;
+    display: block;
+`;
+
 
 export const AddEntryForm = () => {
   // const [show, setShow] = useState(false);
@@ -26,7 +58,8 @@ export const AddEntryForm = () => {
 
   return (
     <>
-            <Form.Group>
+
+            {/* <Form.Group>
               <Form.Label>Name</Form.Label>
               <Form.Control
                 type="text"
@@ -45,7 +78,30 @@ export const AddEntryForm = () => {
               <Form.Text className="text">
                 A short description of the repair request
               </Form.Text>
-            </Form.Group>
+            </Form.Group> */}
+
+            <FormGroup>
+      <Label htmlFor="name">Name</Label>
+      <Input
+        placeholder="Enter name of repair request"
+        value={name}
+        id="name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Message>This is the validation message</Message>
+    </FormGroup>
+    <FormGroup>
+      <Label htmlFor="detail">Details</Label>
+      <Input
+       type="text"
+       as="textarea"
+       placeholder="Details"
+       value={detail}
+       id="detail"
+       onChange={(e) => setDetail(e.target.value)}
+      />
+      <Message>This is the validation message</Message>
+    </FormGroup>
     </>
   );
 };
